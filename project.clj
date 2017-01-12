c58e6c43213c71fc01673dbbcbeda6f2e3c404f0
StevenAvery <Steven.Avery@UOIT.net>
2017-01-12 13:21:10 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject esc "1.0.0-SNAPSHOT"
+  :description "Computes Elementary Cellular Automaton and saves result as a png image."
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main esc.core)
---
+++ test/sec/test/core.clj
@@ -0,0 +1,6 @@
+(ns esc.test.core
+  (:use [esc.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
